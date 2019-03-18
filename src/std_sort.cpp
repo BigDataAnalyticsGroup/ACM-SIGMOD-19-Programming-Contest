9ddff6b86054b135e612137fb9d1b9322b8d1e56
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-03-18 10:40:19 +0100

    Add source and respective header file to implement sorting algorithms.

--- src/std_sort.cpp
+++ src/std_sort.cpp
@@ -1,40 +1,16 @@
-#include "mmap.hpp"
-#include "record.hpp"
-#include <algorithm>
+#include "sort.hpp"
 #include <cstddef>
-#include <cstdint>
 #include <cstdlib>
-#include <cstring>
 #include <iostream>
 
 
-void usage(std::ostream &out, const char *name)
-{
-    out << "USAGE:\n\t" << name << " <FILE.bin>" << std::endl;
-}
-
 int main(int argc, char **argv)
 {
-    if (argc != 2) {
-        usage(std::cerr, argv[0]);
+    if (argc != 3) {
+        std::cerr << "USAGE:\n\t" << argv[0] << " <INPUT-FILE> <OUTPUT-FILE>" << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    /* Map the file into memory. */
-    const char *path = argv[1];
-    MMapFile file(path);
-
-    /* Access the data as array of struct. */
-    auto data = reinterpret_cast<record*>(file.addr());
-    const std::size_t count = file.size() / sizeof(*data);
-
-    /* Sort all records. */
-    std::sort(data, data + count, [](const record &first, const record &second) {
-            return memcmp(first.key, second.key, 10) < 0;
-            });
-
-    /* Write the sorted data to stdout. */
-    std::cout.write(reinterpret_cast<char*>(file.addr()), file.size());
-    std::cout.flush();
+    stl_sort_mmap(argv[1], argv[2]);
     exit(EXIT_SUCCESS);
 }
