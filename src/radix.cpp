8032f68a539ccaeb614c71eaf891bd1c86bb33e9
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-03-22 11:13:55 +0100

    Print time for different parts of partitioning.

--- src/radix.cpp
+++ src/radix.cpp
@@ -29,7 +29,11 @@
 #include "mmap.hpp"
 #include "record.hpp"
 #include <cassert>
+#include <chrono>
 #include <cstdint>
+
+
+using namespace std::chrono;
 
 
 void radix_hist(const char *infile, const char *outfile)
@@ -41,15 +45,19 @@ void radix_hist(const char *infile, const char *outfile)
     const auto begin_in = static_cast<record*>(in.addr());
     const auto end_in = begin_in + num_records;
 
+    const auto t_hist_begin = high_resolution_clock::now();
     /* Compute histogram. */
     histogram_t histogram = hist_MT(begin_in, end_in, 10);
     std::cerr << histogram << std::endl;
+    const auto t_hist_end = high_resolution_clock::now();
+    std::cerr << "t_histogram: " << duration_cast<microseconds>(t_hist_end - t_hist_begin).count() / 1e3 << " ms\n";
 
     /* Allocate output file. */
     MMapFile out(outfile, in.size());
     out.advise(POSIX_FADV_RANDOM);
 
     /* Compute the partition locations. */
+    const auto t_compute_begin = high_resolution_clock::now();
     std::array<record*, NUM_PARTITIONS> partitions;
     unsigned offset = 0;
     for (std::size_t i = 0; i != NUM_PARTITIONS; ++i) {
@@ -57,11 +65,16 @@ void radix_hist(const char *infile, const char *outfile)
         offset += histogram[i];
     }
     assert(offset == num_records);
+    const auto t_compute_end = high_resolution_clock::now();
+    std::cerr << "t_compute: " << duration_cast<microseconds>(t_compute_end - t_compute_begin).count() / 1e3 << " ms\n";
 
     /* Sequentially scan the input data and write it to its respective partitions. */
+    const auto t_write_begin = high_resolution_clock::now();
     for (auto src = begin_in; src != end_in; ++src) {
         const auto pid = src->get_radix_bits();
         record *dst = partitions[pid]++;
         memcpy(dst, src, sizeof(record));
     }
+    const auto t_write_end = high_resolution_clock::now();
+    std::cerr << "t_write: " << duration_cast<microseconds>(t_write_end - t_write_begin).count() / 1e3 << " ms\n";
 }
