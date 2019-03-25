601f781483579bb000d38c075aea3f297038418c
Immanuel Haffner <haffner.immanuel@gmail.com>
2019-03-25 11:15:54 +0100

    Modify benchmark_hist to automatically test all histogram binaries.

--- benchmark_hist.sh
+++ benchmark_hist.sh
@@ -2,26 +2,33 @@
 
 NUM_RUNS=3
 BIN_DIR="build/release/bin"
-FILE="resource/20G.bin"
+FILE="resource/5G.bin"
 
-EXE=$1
-BIN="${BIN_DIR}/${EXE}"
-
-if [ ! -f "$BIN" ];
-then
-    >&2 echo "File does not exist: $BIN"
-    exit 1;
-fi
-
-for i in {1..$NUM_RUNS};
-do
-    echo 1 | sudo tee "/proc/sys/vm/drop_caches" > /dev/null;
-    time "$BIN" "$FILE";
-    RET=$?
-    if [ $RET -ne 0 ];
+function benchmark
+{
+    BIN=$1
+    echo "$BIN"
+    if [ ! -f "$BIN" ];
     then
-        >&2 echo "Program execution failed with status code $RET";
-        exit 1;
+        >&2 echo "File does not exist: $BIN"
+        return 1;
     fi
+
+    for i in {1..$NUM_RUNS};
+    do
+        echo 1 | sudo tee "/proc/sys/vm/drop_caches" > /dev/null;
+        time "$BIN" "$FILE";
+        RET=$?
+        if [ $RET -ne 0 ];
+        then
+            >&2 echo "Program execution failed with status code $RET";
+            exit 1;
+        fi
+    done
+    echo 1 | sudo tee "/proc/sys/vm/drop_caches" > /dev/null;
+}
+
+for BIN in "$BIN_DIR/hist_"*;
+do
+    benchmark "$BIN";
 done
-echo 1 | sudo tee "/proc/sys/vm/drop_caches" > /dev/null;
